042c31554c9b1c3d43da7a9c458edbbe92f65e28
toreapat <torea.patissier@laplateforme.io>
2020-11-03 16:30:41 +0100

    Job4 OK

---
+++ jour02/job04/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$num = 0;
+
+while ($num <=100)
+{
+    echo $num. "<br/>";
+    $num++;
+
+    if (!($num%3)){
+        echo "Fizz";
+    }
+
+    if (!($num%5)){
+        echo "Buzz";
+    }
+
+    elseif ($num %3 == 0 AND $num %5 == 0){
+        echo"FizzBuzz";
+    }
+
+}
+?>
